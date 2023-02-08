d7627eca7b253cd1953ecf016a02f634d35150e9
David Sarria <sarriafreelancer7@gmailcom>
2023-02-08 14:29:14 -0500

    avance skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,26 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skill-content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+    if(itemClass === 'skills__content skills__open'){
+        this.parentNode.className = 'skills__content skills__close'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
