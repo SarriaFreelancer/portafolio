bf26f1ec591246de24bb0aa06b45a7eb4f540089
David Sarria <sarriafreelancer7@gmailcom>
2023-02-09 13:46:30 -0500

    Modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,6 +72,27 @@ tabs.forEach(tab => {
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__buttton'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('modal__active')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
